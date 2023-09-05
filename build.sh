89a92b3de29bf9b20581174221708a4b454fec64
Adrián Castelló <adcastel@uji.es>
2023-09-05 12:26:51 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,37 +8,37 @@ gcc_min=10
 
 if ! hash gcc; then
     echo "ERROR: gcc is not installed"
-    exit 1
+    return 1
 fi
 
 gcc_v=$(gcc --version | grep ^gcc | sed 's/^.* //g' | cut -f1 -d".")
 if [ $gcc_v -lt $gcc_min ]; then
     echo "Update gcc version to 10.0.0 or higher"
-    exit 1
+    return 1
 fi
 echo "[*] gcc OK"
 
 if ! hash python3; then
     echo "python 3 is not installed"
-    exit 1
+    return 1
 fi
 
 ver=$(python3 -V 2>&1 | sed 's/.* \([0-9]\).\([0-9]\).*/\1\2/')
 if [ "$ver" -lt "39" ]; then
     echo "This script requires python 3.9 or greater"
-    exit 1
+    return 1
 fi
 echo "[*] python3 OK"
 
 if ! hash pip3; then
     echo "pip3 is not installed"
-    exit 1
+    return 1
 fi
 echo "[*] pip3 OK"
 
 if ! hash gnuplot; then
     echo "gnuplot is not installed"
-    exit 1
+    return 1
 fi
 echo "[*] gnuplot OK"
 
